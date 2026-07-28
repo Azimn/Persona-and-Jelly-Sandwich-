879c1860ebaba31a58089731355750c5d9abac05
Azimn <jratican@gmail.com>
2026-07-28 10:44:42 -0500

    Export persistent host and room APIs

--- digital_subject/__init__.py
+++ digital_subject/__init__.py
@@ -2,6 +2,23 @@
 
 from .cartridge import Cartridge, load_cartridge
 from .engine import SubjectEngine
+from .host import CatchUpReport, PersistentOrganismHost, RuntimeState
+from .inspector import AwayReport, OrganismInspector
 from .models import Consequence, Event, SubjectState
+from .world import RoomState, RoomWorld
 
-__all__ = ["Cartridge", "Consequence", "Event", "SubjectEngine", "SubjectState", "load_cartridge"]
+__all__ = [
+    "AwayReport",
+    "Cartridge",
+    "CatchUpReport",
+    "Consequence",
+    "Event",
+    "OrganismInspector",
+    "PersistentOrganismHost",
+    "RoomState",
+    "RoomWorld",
+    "RuntimeState",
+    "SubjectEngine",
+    "SubjectState",
+    "load_cartridge",
+]
