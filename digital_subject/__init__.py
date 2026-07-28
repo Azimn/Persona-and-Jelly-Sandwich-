6731d9380c91049e31f5095899ae1e4b11796807
Azimn <jratican@gmail.com>
2026-07-28 18:18:03 -0500

    Export continuity runtime APIs

--- digital_subject/__init__.py
+++ digital_subject/__init__.py
@@ -1,7 +1,16 @@
 """Small deterministic digital-organism runtime."""
 
 from .cartridge import Cartridge, load_cartridge
+from .continuity import (
+    Commitment,
+    ContinuityState,
+    EpistemicRecord,
+    Expectation,
+    ReflectionInsight,
+    SubjectContinuity,
+)
 from .engine import SubjectEngine
+from .enhanced_host import PersistentContinuityHost
 from .host import CatchUpReport, PersistentOrganismHost, RuntimeState
 from .inspector import AwayReport, OrganismInspector
 from .models import Consequence, Event, SubjectState
@@ -11,13 +20,20 @@ __all__ = [
     "AwayReport",
     "Cartridge",
     "CatchUpReport",
+    "Commitment",
     "Consequence",
+    "ContinuityState",
+    "EpistemicRecord",
     "Event",
+    "Expectation",
     "OrganismInspector",
+    "PersistentContinuityHost",
     "PersistentOrganismHost",
+    "ReflectionInsight",
     "RoomState",
     "RoomWorld",
     "RuntimeState",
+    "SubjectContinuity",
     "SubjectEngine",
     "SubjectState",
     "load_cartridge",
